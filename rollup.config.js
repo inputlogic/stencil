e9c0a66365086da19eea4c3443bca1b0b450363c
Adriaan Mulder <adriaan.mulder@gmail.com>
2023-10-22 10:12:44 -0700

    Add dist to gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,19 @@ import { babel } from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 import pkg from './package.json' assert { type: "json" }
 
 export default [
   {
     input: 'src/index.js',
-    external: [
-      'react',
-      '@tanstack/react-query',
-      'react-hook-form',
-    ],
     output: [
       {file: pkg.main, format: 'cjs', sourcemap: true, },
       {file: pkg.module, format: 'es', sourcemap: true}
     ],
     plugins: [
+      peerDepsExternal(),
       postcss({
         inject: true
       }),
